d16a94e37973d9f99b8ca723424f800228839f0d
kanitvural <kanitvural@gmail.com>
2024-12-04 10:58:40 +0300

    up

--- app.py
+++ app.py
@@ -59,6 +59,3 @@ if 'ocr_result' in st.session_state:
 else:
     st.info("Upload an image and click 'Extract Text' to see the results here.")
 
-# Footer
-st.markdown("---")
-st.markdown("Made with ❤️ using Llama Vision Model2 | [Report an Issue](https://github.com/patchy631/ai-engineering-hub/issues)")
